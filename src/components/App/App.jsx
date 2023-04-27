10c1a0853500964aa832a7a5dd45f203ea63ec56
Claire De Vito <claire.devito@gmail.com>
2023-04-27 10:36:33 +0200

    Initialisation du projet

---
+++ src/components/App/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from '../../pages/Home/Home'
+import Error from '../../pages/Error/Error'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+import '../../style/global-style.css'
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
